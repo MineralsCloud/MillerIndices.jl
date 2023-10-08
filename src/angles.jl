c4269786b4aba1cb9d942ec321d6d39a7c6d46b6
singularitti <singularitti@outlook.com>
2023-10-07 21:01:51 -0400

    Deprecate `directionangle`, use `angle`

--- src/angles.jl
+++ src/angles.jl
@@ -1,22 +1,14 @@
 using CrystallographyBase: MetricTensor
 using LinearAlgebra: norm, dot
 
-export angle, directionangle, interplanar_spacing
+export angle, interplanar_spacing
 
 """
     angle(𝐚::AbstractVector, g::MetricTensor, 𝐛::AbstractVector)
 
-Get the angle between two directions.
+Get the angle (in degrees) between two directions.
 """
-angle(𝐚, g::MetricTensor, 𝐛) = dot(𝐚, g, 𝐛) / (norm(𝐚, g) * norm(𝐛, g))
-
-"""
-    directionangle(𝐚::AbstractVector, g::MetricTensor, 𝐛::AbstractVector)
-
-Get the direction angle of two vectors and a `MetricTensor`.
-"""
-directionangle(𝐚::AbstractVector, g::MetricTensor, 𝐛::AbstractVector) =
-    acosd(angle(𝐚, g, 𝐛))
+angle(𝐚, g::MetricTensor, 𝐛) = acosd(dot(𝐚, g, 𝐛) / (norm(𝐚, g) * norm(𝐛, g)))
 
 """
     interplanar_spacing(𝐚::AbstractVector, g::MetricTensor)
